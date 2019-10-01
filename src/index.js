a8e1d43eaf9faa40ff96c2c7b80a19c9835c5ba2
Alex-Edward_Klim <alexklim3221128@gmail.com>
2019-10-01 16:36:25 +0300

    feat: add solution to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstArr = first.split('');
+    for (let i = 0; i < firstArr.length; i++) {
+        firstArr[i] = Number(firstArr[i]);
+    }
+
+    let secondArr = second.split('');
+    for (let j = 0; j < secondArr.length; j++) {
+        secondArr[j] = Number(secondArr[j]);
+    }
+
+    let topArr = [];
+    let bottomArr = [];
+    if (firstArr.length > secondArr.length) {
+        topArr = firstArr;
+        bottomArr = secondArr;
+    } else {
+        topArr = secondArr;
+        bottomArr = firstArr;
+    }
+
+    topArr.reverse();
+    bottomArr.reverse();
+
+    let finalArr = [];
+    let keepInMinder = 0;
+    let num = 0;
+
+    for (let m = 0; m < bottomArr.length; m++) {
+        for (let n = 0; n < topArr.length; n++) {
+
+            num = bottomArr[m] * topArr[n] + keepInMinder;
+            
+
+            if (num > 9) {
+                keepInMinder = Number(String(num)[0]);
+                num = Number(String(num)[1]);
+            } else {
+                keepInMinder = 0;
+            }
+            
+
+            finalArr[m + n] = finalArr[m + n] + num || num;
+            
+            if (finalArr[m + n] > 9) {
+                finalArr[m + n + 1] = finalArr[m + n + 1] + Number(String(finalArr[m + n])[0]) || Number(String(finalArr[m + n])[0]);
+                finalArr[m + n] = Number(String(finalArr[m + n])[1]);
+            }
+           
+            if (n == topArr.length - 1) {
+                finalArr[m + n + 1] = finalArr[m + n + 1] + keepInMinder || keepInMinder;
+                keepInMinder = 0;
+            }
+
+        }
+    }
+
+    let extraZero = finalArr.length - 1;
+    while (finalArr[extraZero] == 0) {
+        finalArr.pop();
+        extraZero -= 1;
+    }
+
+    finalArr.reverse();
+
+    return finalArr.join('');
 }
